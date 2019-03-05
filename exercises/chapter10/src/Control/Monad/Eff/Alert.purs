0eac775844dd4e1cb3737ee9592dc9ae46e1bf19
Schlecht <volker@schlecht.org>
2019-03-05 20:16:21 +0800

    Drop NPM and Bower dependencies; Move everything from Control.Monad.Eff to Effect

--- exercises/chapter10/src/Control/Monad/Eff/Alert.purs
+++
@@ -1,9 +0,0 @@
-module Control.Monad.Eff.Alert where
-
-import Prelude
-
-import Control.Monad.Eff (kind Effect, Eff)
-
-foreign import data ALERT :: Effect
-
-foreign import alert :: forall eff. String -> Eff (alert :: ALERT | eff) Unit
