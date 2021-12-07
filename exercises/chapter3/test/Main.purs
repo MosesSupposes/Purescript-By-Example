195edd49018d89522720befd067196d05c0cd3eb
MosesSupposes <mosesasamuel633@gmail.com>
2021-12-06 23:39:17 -0600

    solve exercises for chapter 3

--- exercises/chapter3/test/Main.purs
+++ exercises/chapter3/test/Main.purs
@@ -48,18 +48,16 @@ otherJohn =
       { street: "678 Fake Rd.", city: "Fakeville", state: "NY" }
   }
 
-
 bookWithDuplicate :: AddressBook
 bookWithDuplicate =
   insertEntry john
     $ insertEntry otherJohn
-      book
+        book
 
 main :: Effect Unit
 main =
   runTest do
     runChapterExamples
-    {-  Move this block comment starting point to enable more tests
     suite "Exercise - findEntryByStreet" do
       test "Lookup existing" do
         Assert.equal (Just john)
@@ -78,6 +76,7 @@ main =
       Assert.equal book
         $ removeDuplicates bookWithDuplicate
 
+{-  Move this block comment starting point to enable more tests
 -}
 runChapterExamples :: TestSuite
 runChapterExamples = do
--- exercises/chapter3/test/MySolutions.purs
+++ exercises/chapter3/test/MySolutions.purs
@@ -1,5 +1,42 @@
 module Test.MySolutions where
 
 import Prelude
+import Data.AddressBook
+import Data.List (filter, head, (:), List(..), nubByEq)
+import Data.Maybe (Maybe(..))
+
+type FirstName
+  = String
+
+type LastName
+  = String
 
 -- Note to reader: Add your solutions to this file
+findEntryByStreet :: String -> AddressBook -> Maybe Entry
+findEntryByStreet street = head <<< filter filterEntry
+  where
+  filterEntry :: Entry -> Boolean
+  filterEntry entry = entry.address.street == street
+
+findEntryByStreet' :: String -> AddressBook -> Maybe Entry
+findEntryByStreet' street = head <<< filter (_.address.street >>> eq street)
+
+isInBook :: FirstName -> LastName -> AddressBook -> Boolean
+isInBook fName lName book = case findEntry fName lName book of
+  Just _ -> true
+  Nothing -> false
+
+isInBook' :: FirstName -> LastName -> AddressBook -> Boolean
+isInBook' fName lName book = wasFound book
+  where
+  wasFound :: AddressBook -> Boolean
+  wasFound remainingContacts = case remainingContacts of
+    Nil -> false
+    contact : contacts ->
+      if contact.firstName == fName && contact.lastName == lName then
+        true
+      else
+        wasFound contacts
+
+removeDuplicates :: AddressBook -> AddressBook
+removeDuplicates book = nubByEq (\book1 book2 -> book1.firstName == book2.firstName && book1.lastName == book2.lastName) book
